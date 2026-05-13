f3f01cf8568826f778256e50a68d7b8ba2e73a27
retz8 <ekkicb71@gmail.com>
2026-05-13 19:58:22 +0900

    chapter 2: guessing game proj

---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,18 @@
+// 1. ask for user input
+// 2. process that input
+// 3. check that the input is in the expected form
+
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
