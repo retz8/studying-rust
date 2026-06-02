9469e6261f01096b23482f351f6162be50379640
retz8 <ekkicb71@gmail.com>
2026-06-02 19:53:13 +0900

    chore: p4 minor code change

--- projects/4_reference_borrowing/src/main.rs
+++ projects/4_reference_borrowing/src/main.rs
@@ -21,12 +21,15 @@
 //     (g1, g2)
 // }
 
-let mut x: Box<i32> = Box::new(1);
-let a: i32 = *x; // *x reads the heap value of x, meaning 1
-*x += 1; // x points to value 2 now
+fn main() {
+    let mut x: Box<i32> = Box::new(1);
+    let a: i32 = *x; // *x reads the heap value of x, meaning 1
+    *x += 1; // x points to value 2 now
 
-let r1: &Box<i32> = &x; // r1 points to x on the stack
-let b: i32 = **r1; // meaning 1
+    let r1: &Box<i32> = &x; // r1 points to x on the stack
+    let b: i32 = **r1; // meaning 1
 
-let r2: &i32 = &*x;
-let c: i32 = *r2;
+    let r2: &i32 = &*x;
+    let c: i32 = *r2;
+}
+
